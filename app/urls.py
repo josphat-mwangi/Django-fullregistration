b5c6237e566635afb5f407ea1411aed5ec691565
Josphat Mwangi <josphatwanjiruw@gmail.com>
2021-07-04 22:51:56 +0300

    create registration view and add urls for reset and changing password

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('', views.register, name='register')
+    path('', views.register, name='register'),
+    path('home/', views.home, name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('password_change/',auth_views.PasswordChangeView.as_view(template_name='registration/password_change-form.html'), name='password_change' ),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password-change-done.html'),  name='password_change_done' ),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'),name='password_reset' ),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password-reset-done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password-reset-confirm.html"), name='password_reset_confirm' ),
+    path('reset/done/', auth_views. PasswordResetCompleteView.as_view(template_name="registration/password-reset-complete.html"), name='password_reset_complete')
+
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 
 
 def register(request):
-    return render(request, 'registration/register.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            raw_password = form.cleaned_data.get('password1')
+            username = form.cleaned_data.get('username')
+            user = authenticate(username=username, password =raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+
+
+    return render(request, 'registration/register.html', {'form':form})
+    
+    
+    
+def home(request):
+    return render(request, 'index.html')
